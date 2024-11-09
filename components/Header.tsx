7e71aa7b187308d55ec0663b8c69ab932c853446
umair khan <umairkhans19@yahoo.com>
2024-11-09 18:34:20 +0500

    Assignment Done

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-[#A29875] flex items-center p-4 shadow-md">
+      
+      <div className="flex items-center">
+        
+        <span className="text-white text-[55px] font-bold">MANZZARI</span>
+      </div>
+
+    
+      
+    </header>
+  );
+}
+
